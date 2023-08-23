286ba1c3af1218821f04d0a72220b84ac331a8e7
Sun-2302 <hei.dera.932@gmail.com>
2023-08-23 14:59:56 +0300

    feat: base structure for Spring Boot

---
+++ java-absence-management/src/main/java/school/hei/javaabsencemanagement/JavaAbsenceManagementApplication.java
@@ -0,0 +1,13 @@
+package school.hei.javaabsencemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAbsenceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAbsenceManagementApplication.class, args);
+	}
+
+}
